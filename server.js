e882bbc6d7ef017b1180c931f5182155140aa8c2
hagler mijail <hagler.mijail@gmail.com>
2017-06-15 10:37:47 -0500

    update

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 // var http = require("http")
 var fs = require("fs")
+var chp5 = require ('./src/chapter5Queues')
 
+
+var data5txt = []
 fs.readFile('./src/chapter3files.txt', 'utf8', function(err, data) {
     if (err) throw err;
-    console.log(data);
+    data5txt = data
+    console.log(data5txt.chp5.toString());
 });
+
+
 
 
 // NOTE: EXECUTE this file with node server.js in console
--- src/chapter5Queues.js
+++ src/chapter5Queues.js
@@ -12,6 +12,7 @@
 'use strict'
 
 function Queue() {
+  this.file = []
   this.dataStore = []
   this.enqueue = enqueue
   this.dequeue = dequeue
@@ -19,6 +20,7 @@ function Queue() {
   this.back = back
   this.toString = toString
   this.empty = empty
+
 }
 
 function enqueue(element) {
@@ -70,12 +72,105 @@ console.log("after dequeue: "+ toString(q));
 console.log('show FRONT of queue: '+ q.front());
 console.log('show BACK of queue: '+ q.back());
 
-
+console.log();
 // NOTE: USING THE QUEUE CLASS : ASSIGNING PARTNERS AT A SQUARE DANCE
 
-var fs = require('fs');
+function Dancer(name, sex) {
+  this.name = name
+  this.sex = sex
+}
 
-fs.readFile('my-file.txt', 'utf8', function(err, data) {
-    if (err) throw err;
-    console.log(data);
-});
+function getDancers(males, females) {
+  var names = readtext.split("\n")
+  for (var i = 0; i < names.length; i++) {
+    names[i] = names[i].trim()
+  }
+
+  for (var i = 0; i < names.length; i++) {
+    var dancer = names[i].split(" ")
+    var sex = dancer[0]
+    var name = dancer[1]
+    if (sex == "F") {
+      females.enqueue(new Dancer(name, sex))
+    }
+    else {
+      males.enqueue(new Dancer(name, sex))
+    }
+  }
+}
+
+function dance(males, females) {
+  console.log("The dance partners are: \n");
+  while (!females.empty() && !males.empty()){
+    person = females.dequeue()
+    console.log("Female dancer is: " + person.name);
+    person = males.dequeue()
+    console.log("and the male dancer is: "+ person.name);
+  }
+
+}
+
+//test program
+
+
+
+//
+// var fs = require('fs');
+// var readtext = []
+//
+// fs.readFile('./chapter5.txt', 'utf8', function(err, readtext) {
+//     if (err) throw err;
+//     console.log(readtext);
+//     console.log("typeof: " + typeof readtext);
+//     var maleDancers = new Queue()
+//     var femaleDancers = new Queue()
+//
+//     getDancers(maleDancers, femaleDancers)
+//     dance(maleDancers, femaleDancers)
+//
+//     if (!femaleDancers.empty()) {
+//       console.log(femaleDancers.front().name + " is waiting to dance.");
+//     }
+//
+//     if (!maleDancers.empty()) {
+//       console.log(maleDancers.front().name + " is waitin to dance.");
+//     }
+//
+// });
+
+
+// NOTE: SORTING DATA WITH QUEUES
+// >>> also to be used to sort data
+// in this example we sort the numbers for two types of sort with queues
+
+//SORT BY 1s digits (primer digito, unidades)
+//SORT BY 10s digits (segundo digito, decenas)
+
+
+//this function distributes numbers by the place (1s or 10s)
+function distribute(nums, queues, n, digit) {
+  for (var i = 0; i < n; i++) {
+    if (digit == 1) {
+      queues[nums[i]%10].enqueue(nums[i])
+    }else {
+      queues[Math.floor(nums[i]/10)].enqueue(num[i])
+    }
+  }
+}
+
+//this function collecting numbers from the queues
+
+function collect(queues, nums) {
+  var i = 0
+  for (var digit = 0; digit < 10 ; i++) {
+    while(!queues[digit].empty()){
+      nums[i++] = queues[digit].equeue()
+    }
+  }
+}
+
+function dispArray(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i] + " ");
+  }
+}
