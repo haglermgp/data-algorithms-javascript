4b73c0bc9cee50b30e7cd8ae49543c2f8f8a0552
hagler mijail <hagler.mijail@gmail.com>
2017-07-03 01:56:37 -0500

    update chapter 6

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 // import chapter2 from './src/chapter2'
 // import chapter3 from './src/chapter3'
 // import chapter4Stacks from './src/chapter4Stacks'
-import chapter5Queues from './src/chapter5Queues'
+// import chapter5Queues from './src/chapter5Queues'
+import chapter6LinkedList from './src/chapter6LinkedList'
